9bba596791c0fe5e5dbce76570568c79ae007e19
Peter Sussman <the8thbit@gmail.com>
2015-01-07 22:53:59 -0600

    added preload update and refactored slightly

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-	var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-	function preload () {
+	function preload() {
 		game.load.image('logo', './assets/img/phaser.png');
 	}
 
-	function create () {
+	function create() {
 		var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
 		logo.anchor.setTo(0.5, 0.5);
 	}
